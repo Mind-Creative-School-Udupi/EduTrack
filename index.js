b0cc0f95eb9283df4a73f38d198d874b39d96dcc
rajani-giliyar <119439361+rajani-giliyar@users.noreply.github.com>
2024-08-05 22:06:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,346 @@
+let students = [
+  { studentName: "Ram", studentAge: 20, studentCity: "Udupi", totalMarks: 78 },
+  {
+    studentName: "Radha",
+    studentAge: 23,
+    studentCity: "Bangalore",
+    totalMarks: 85,
+  },
+  {
+    studentName: "Raju",
+    studentAge: 24,
+    studentCity: "Mangalore",
+    totalMarks: 90,
+  },
+  {
+    studentName: "Samay",
+    studentAge: 22,
+    studentCity: "Mumbai",
+    totalMarks: 76,
+  },
+  {
+    studentName: "Sanju",
+    studentAge: 23,
+    studentCity: "Delhi",
+    totalMarks: 88,
+  },
+  {
+    studentName: "Rakesh",
+    studentAge: 26,
+    studentCity: "Pune",
+    totalMarks: 72,
+  },
+  {
+    studentName: "Umesh",
+    studentAge: 20,
+    studentCity: "Chennai",
+    totalMarks: 80,
+  },
+  {
+    studentName: "Uday",
+    studentAge: 29,
+    studentCity: "Hyderabad",
+    totalMarks: 69,
+  },
+  {
+    studentName: "Bhavya",
+    studentAge: 21,
+    studentCity: "Udupi",
+    totalMarks: 95,
+  },
+  {
+    studentName: "Chirag",
+    studentAge: 20,
+    studentCity: "Kolkata",
+    totalMarks: 74,
+  },
+  { studentName: "Lipi", studentAge: 20, studentCity: "Udupi", totalMarks: 82 },
+  {
+    studentName: "Amit",
+    studentAge: 22,
+    studentCity: "Bangalore",
+    totalMarks: 87,
+  },
+  { studentName: "Nisha", studentAge: 23, studentCity: "Pune", totalMarks: 79 },
+  {
+    studentName: "Rohan",
+    studentAge: 25,
+    studentCity: "Mumbai",
+    totalMarks: 85,
+  },
+  {
+    studentName: "Sneha",
+    studentAge: 21,
+    studentCity: "Chennai",
+    totalMarks: 92,
+  },
+  {
+    studentName: "Anjali",
+    studentAge: 24,
+    studentCity: "Hyderabad",
+    totalMarks: 81,
+  },
+  {
+    studentName: "Karan",
+    studentAge: 27,
+    studentCity: "Delhi",
+    totalMarks: 77,
+  },
+  {
+    studentName: "Pooja",
+    studentAge: 20,
+    studentCity: "Mangalore",
+    totalMarks: 83,
+  },
+  {
+    studentName: "Vikram",
+    studentAge: 23,
+    studentCity: "Udupi",
+    totalMarks: 80,
+  },
+  {
+    studentName: "Rita",
+    studentAge: 22,
+    studentCity: "Bangalore",
+    totalMarks: 88,
+  },
+  {
+    studentName: "Suresh",
+    studentAge: 25,
+    studentCity: "Mangalore",
+    totalMarks: 90,
+  },
+  {
+    studentName: "Geeta",
+    studentAge: 21,
+    studentCity: "Mumbai",
+    totalMarks: 70,
+  },
+  {
+    studentName: "Arjun",
+    studentAge: 24,
+    studentCity: "Delhi",
+    totalMarks: 78,
+  },
+  {
+    studentName: "Naveen",
+    studentAge: 26,
+    studentCity: "Pune",
+    totalMarks: 74,
+  },
+  {
+    studentName: "Neha",
+    studentAge: 20,
+    studentCity: "Chennai",
+    totalMarks: 82,
+  },
+  {
+    studentName: "Rajesh",
+    studentAge: 29,
+    studentCity: "Hyderabad",
+    totalMarks: 68,
+  },
+  {
+    studentName: "Priya",
+    studentAge: 21,
+    studentCity: "Udupi",
+    totalMarks: 91,
+  },
+  {
+    studentName: "Manoj",
+    studentAge: 20,
+    studentCity: "Kolkata",
+    totalMarks: 77,
+  },
+  {
+    studentName: "Meena",
+    studentAge: 20,
+    studentCity: "Udupi",
+    totalMarks: 75,
+  },
+  {
+    studentName: "Varun",
+    studentAge: 22,
+    studentCity: "Bangalore",
+    totalMarks: 89,
+  },
+  {
+    studentName: "Rashmi",
+    studentAge: 23,
+    studentCity: "Pune",
+    totalMarks: 80,
+  },
+  {
+    studentName: "Nikhil",
+    studentAge: 25,
+    studentCity: "Mumbai",
+    totalMarks: 86,
+  },
+  {
+    studentName: "Pallavi",
+    studentAge: 21,
+    studentCity: "Chennai",
+    totalMarks: 93,
+  },
+  {
+    studentName: "Deepak",
+    studentAge: 24,
+    studentCity: "Hyderabad",
+    totalMarks: 82,
+  },
+  {
+    studentName: "Vijay",
+    studentAge: 27,
+    studentCity: "Delhi",
+    totalMarks: 76,
+  },
+  {
+    studentName: "Tina",
+    studentAge: 20,
+    studentCity: "Mangalore",
+    totalMarks: 84,
+  },
+  { studentName: "Ajay", studentAge: 23, studentCity: "Udupi", totalMarks: 81 },
+  {
+    studentName: "Kavya",
+    studentAge: 22,
+    studentCity: "Bangalore",
+    totalMarks: 78,
+  },
+  {
+    studentName: "Sanjay",
+    studentAge: 25,
+    studentCity: "Mangalore",
+    totalMarks: 91,
+  },
+  {
+    studentName: "Leela",
+    studentAge: 21,
+    studentCity: "Mumbai",
+    totalMarks: 71,
+  },
+  {
+    studentName: "Gopal",
+    studentAge: 24,
+    studentCity: "Delhi",
+    totalMarks: 79,
+  },
+  { studentName: "Ravi", studentAge: 26, studentCity: "Pune", totalMarks: 73 },
+  {
+    studentName: "Meera",
+    studentAge: 20,
+    studentCity: "Chennai",
+    totalMarks: 81,
+  },
+  {
+    studentName: "Sudha",
+    studentAge: 29,
+    studentCity: "Hyderabad",
+    totalMarks: 67,
+  },
+  {
+    studentName: "Suman",
+    studentAge: 21,
+    studentCity: "Udupi",
+    totalMarks: 90,
+  },
+  {
+    studentName: "Akash",
+    studentAge: 20,
+    studentCity: "Kolkata",
+    totalMarks: 76,
+  },
+  {
+    studentName: "Kiran",
+    studentAge: 20,
+    studentCity: "Udupi",
+    totalMarks: 75,
+  },
+  {
+    studentName: "Vikas",
+    studentAge: 22,
+    studentCity: "Bangalore",
+    totalMarks: 86,
+  },
+  {
+    studentName: "Preeti",
+    studentAge: 23,
+    studentCity: "Pune",
+    totalMarks: 81,
+  },
+];
+let filterStudent = students;
+let currentPage = 1;
+
+function updatePagination() {
+  let noOfPages = Math.ceil(filterStudent.length / 5);
+  let nums = document.getElementById("nums");
+  nums.innerHTML = "";
+
+  for (let i = 1; i <= noOfPages; i++) {
+    let btn = document.createElement("button");
+    btn.innerText = i;
+    btn.addEventListener("click", () => {
+      currentPage = i;
+      paginate(currentPage);
+    });
+    nums.appendChild(btn);
+  }
+
+  document.getElementById("next").disabled = currentPage >= noOfPages;
+  document.getElementById("previous").disabled = currentPage <= 1;
+  paginate(currentPage);
+}
+
+document.getElementById("search").addEventListener("keyup", () => {
+  let val = document.getElementById("search").value;
+  filterStudent = students.filter((student) => {
+    return student.studentName.includes(val);
+  });
+  currentPage = 1;
+  updatePagination();
+});
+
+document.getElementById("next").addEventListener("click", function () {
+  let noOfPages = Math.ceil(filterStudent.length / 5);
+  if (currentPage < noOfPages) {
+    currentPage++;
+    paginate(currentPage);
+  }
+});
+
+document.getElementById("previous").addEventListener("click", function () {
+  if (currentPage > 1) {
+    currentPage--;
+    paginate(currentPage);
+  }
+});
+
+// initial call
+updatePagination();
+
+function paginate(pageNumber) {
+  let noOfPages = Math.ceil(filterStudent.length / 5);
+  let startIndex = (pageNumber - 1) * 5;
+  let endIndex = startIndex + 5;
+
+  let paginatedData = filterStudent.slice(startIndex, endIndex);
+  document.getElementById("info").innerText = currentPage + " of " + noOfPages;
+  document.getElementById("tbody").innerHTML = "";
+  for (let i = 0; i < paginatedData.length; i++) {
+    let row = document.createElement("tr");
+    let noTd = document.createElement("td");
+    noTd.innerText = startIndex + i + 1;
+    let nameTd = document.createElement("td");
+    nameTd.innerText = paginatedData[i].studentName;
+    let ageTd = document.createElement("td");
+    ageTd.innerText = paginatedData[i].studentAge;
+    let cityTd = document.createElement("td");
+    cityTd.innerText = paginatedData[i].studentCity;
+    let marksTd = document.createElement("td");
+    marksTd.innerText = paginatedData[i].totalMarks;
+
+    row.append(noTd, nameTd, ageTd, cityTd, marksTd);
+    document.getElementById("tbody").appendChild(row);
+  }
+}
